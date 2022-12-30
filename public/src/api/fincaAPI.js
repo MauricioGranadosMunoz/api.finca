1b0116d86bc8bd5a86ef83714bcf86e50f82b30a
Jose Mauricio Granados Muñoz <josgrana1@publicisgroupe.net>
2022-12-30 14:22:28 -0600

    sda

--- public/src/api/fincaAPI.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const usuarioApi = axios.create({
-  baseURL: "http://192.168.100.10/FincaBackEnd/v1/usuarios",
-});
-export const cajuelaApi = axios.create({
-  baseURL: "http://192.168.100.10/FincaBackEnd/v1/cajuelas",
-});
--- public/src/store/slices/Usuario/index.js
+++
@@ -1,2 +0,0 @@
-export * from "./usuarioSlice";
-export * from "./usuarioThunk";
