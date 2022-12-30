35a9e082939acb6211bbc8ec1bcea65e635ce59a
Jose Mauricio Granados Muñoz <josgrana1@publicisgroupe.net>
2022-12-30 10:15:23 -0600

    d

--- public/src/pages/components/DinamicCajuelasForm.jsx
+++ public/src/pages/components/DinamicCajuelasForm.jsx
@@ -112,6 +112,29 @@ export const DinamicCajuelasForm = ({ cajuelaSemana, precioActual }) => {
     return <CheckingAuth />;
   }
 
+  const addFields = () => {
+      let newfield =
+      {
+        "lunes": 0,
+        "martes": 0,
+        "miercoles": 0,
+        "jueves": 0,
+        "viernes": 0,
+        "sabado": 0,
+        "total": 0
+      }
+  
+      setFormFields([...formFields, newfield])
+      dispatch(addCajuelaSemana([...formFields, newfield], usuario));
+  }
+
+  const removeFields = (index) => {
+    let data = [...formFields];
+    data.splice(index, 1)
+    setFormFields(data)
+    dispatch(addCajuelaSemana(data, usuario));
+  }
+
   const RebajasTotalesCalc = (trabajarID) => {
     const RebajasTotales = [];
     rebajas.map((rebaja) => {
@@ -474,6 +497,43 @@ export const DinamicCajuelasForm = ({ cajuelaSemana, precioActual }) => {
           );
         })}
       </div>
+      <Button.Group className="mt-5">
+        <Button onClick={addFields} color="success">
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4"
+                />
+              </svg>
+              Agregar Nuevo Trabajador
+            </Button>
+
+            <Button onClick={removeFields} color="failure">
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4"
+                />
+              </svg>
+              Remover Trabajador
+            </Button>
+          </Button.Group>
     </form>
   );
 };
