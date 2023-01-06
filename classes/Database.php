bca59618dde8d46ef4f061728f573e5bf8d2a16e
Jose Mauricio Granados Muñoz <josgrana1@publicisgroupe.net>
2023-01-06 08:50:30 -0600

    Addin

--- classes/Database.php
+++ classes/Database.php
@@ -12,7 +12,6 @@ class Database {
         try{
             $conn = new PDO('mysql:host='.$this->db_host.';dbname='.$this->db_name,$this->db_username,$this->db_password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo json_encode("Todo bien");
             return $conn;
         }
         catch(PDOException $e){
